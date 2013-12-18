c8f07a35fb278c661ce0deee4bd7930f639fcca4
James Hurford <terrasea@gmail.com>
2013-12-18 23:30:24 +1300

    using 0.0.0.0 as string not InternetAddress.ANY_IP_V4

--- bin/main.dart
+++ bin/main.dart
@@ -6,7 +6,7 @@ main(args) {
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 9999 : int.parse(portEnv);
 
-  HttpServer.bind(InternetAddress.ANY_IP_V4, port).then((server) {
+  HttpServer.bind('0.0.0.0', port).then((server) {
     new Router(server)
       ..serve('/').listen(serveFile('../out/web/rpghelper.xml'))
       ..serve('../out/web/rpghelper.css').listen(serveFile('../out/web/rpghelper.xml'));
