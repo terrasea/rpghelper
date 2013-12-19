96777c8f7ea6b6f1a017d38a0a21087e1629e4d5
James Hurford <terrasea@gmail.com>
2013-12-19 17:44:03 +1300

    Added mime library and modified serveFile to use this
    This save me from a huge amount of work.

--- bin/utils.dart
+++ bin/utils.dart
@@ -1,9 +1,18 @@
 import 'dart:io';
+import 'package:mime/mime.dart';
 
 Function serveFile(String filename) {
+  String mimeType = lookupMimeType(filename);
 
+  String type = "text";
+  String subType = "plain";
+
+  if(mimeType != null) {
+    type = mimeType.split("/")[0];
+    subType = mimeType.split("/")[1];
+  }
   return (HttpRequest request) {
-    request.response.headers.contentType = new ContentType(getType(filename), getSubtype(filename));
+    request.response.headers.contentType = new ContentType(type, subType);
     new File(filename).openRead().pipe(request.response);
   };
 }
@@ -30,6 +39,10 @@ String getSubtype(String filename) {
 
   if(filename.endsWith('xml')) {
     type = "xml";
+  }
+
+  if(filename.endsWith("js")) {
+    type = "javascript";
   }
 
   return type;
